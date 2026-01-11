14447d699bed0782213eb65e9fa0102c7febe756
Beacon Home Tutoring <dawoodcreations906@gmail.com>
2026-01-12 02:39:29 +0500

    Update ESLint configuration and replace deprecated React Helmet package with react-helmet-async

--- beacon-academy/src/main.jsx
+++ beacon-academy/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { HelmetProvider } from '@dr.pogodin/react-helmet';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
